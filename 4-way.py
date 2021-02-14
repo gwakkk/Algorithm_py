27d886307d16f3e67e75744f52085a7fc47dd844
CHANWOO GWAK <65958776+gwakkk@users.noreply.github.com>
2021-02-14 18:40:31 +0900

    21.02.14

---
+++ 4-way.py
@@ -0,0 +1,19 @@
+n= int(input("N을 입력 : "))
+x, y = 1
+plans = input().split()
+
+dx = [0, 0, -1 ,1]
+dy = [-1, 1, 0, 0]
+mov_types = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(mov_types)):
+        if plan == mov_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        if nx < 1 or ny < 1 or nx > n or ny > n:
+            continue
+
+        x, y = nx, ny
+
+print(nx, ny)
