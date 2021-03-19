c56ceab924c37c81fbd8c32cdd1f6d5b6cc26774
CHANWOO GWAK <65958776+gwakkk@users.noreply.github.com>
2021-03-19 23:46:08 +0900

    dijkstra

--- shortest_path/djikstra.py
+++ shortest_path/djikstra.py
@@ -30,3 +30,29 @@ def get_smallest_node():
             index = i
     return index
     
+def dijkstra(start):
+    # 시작 노드 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 n-1개 노드에 대해서 반복한다
+    for i in range(n-1):
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            #현재 노드를 거쳐서 다른 노드로 이동하는 거리가 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    # 도달 불가의 경우 무한 출력
+    if distance[i] == INF:
+        print("Infinity")
+    # 도달하면 거리 출력
+    else:
+        print(distance[i])
